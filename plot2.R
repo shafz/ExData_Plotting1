3d0b1aab338c9a3e6c674716fe4551e42d0aab0f
shaf <mr.shafaizal@gmail.com>
2016-07-01 00:23:02 +0800

    add plot2 script

--- plot2.R
+++ plot2.R
@@ -4,9 +4,10 @@ data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="
 
 # Preparing the data
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 
-# Plot 1
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+# Plot 2
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
